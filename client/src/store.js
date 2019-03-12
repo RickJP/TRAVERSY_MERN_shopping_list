09f6ad36ee1b9424377ed80ac833965e36e46adf
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-12 10:37:17 +0900

    modified store.js

--- client/src/store.js
+++ client/src/store.js
@@ -3,24 +3,14 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-const middleware = [thunk];
 
+const middleWare = [thunk];
 
-// const composeEnhancers = typeof window === 'object' && 
-// window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ ?
-//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose
-const store = createStore(rootReducer, initialState, composeEnhancers(
-    applyMiddleware(...middleware)
-));
-
-
-// const store = createStore(rootReducer, initialState, compose(
-//   applyMiddleware(...middleWare),
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// ));
-
-console.log(store.getState());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleWare))
+);
 
 export default store;
