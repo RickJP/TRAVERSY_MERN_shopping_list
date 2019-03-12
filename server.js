f8208ad007a30aa6e9ceaf0623b11f1603e1d933
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-12 09:10:39 +0900

    Version 1.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const items = require('./routes/api/items');
@@ -9,6 +10,16 @@ const items = require('./routes/api/items');
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // DB Config
 const db = require('./config/keys').mongoURI;
